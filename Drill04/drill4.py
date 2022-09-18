cd791ddc9d7ac1fe7502f6e8aceb52b4caa6db95
Sun@MainPC <blaqluv1015@gmail.com>
2022-09-18 15:40:37 +0900

    Submit Drill04
    Create and Submit Dill04

---
+++ Drill04/drill4.py
@@ -0,0 +1,79 @@
+import turtle
+
+count1=5
+
+while (count1>0):
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(180)
+    turtle.forward(100)
+    turtle.right(90)
+    count1=count1-1
+
+turtle.forward(100)
+count2=5
+
+while(count2>0):
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(180)
+    count2=count2-1
+
+turtle.forward(100)
+count1=5
+
+while (count1>0):
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(180)
+    turtle.forward(100)
+    turtle.right(90)
+    count1=count1-1
+    
+turtle.forward(100)
+count2=5
+
+while(count2>0):
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(180)
+    count2=count2-1
+
+turtle.forward(100)
+count1=5
+
+while (count1>0):
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(180)
+    turtle.forward(100)
+    turtle.right(90)
+    count1=count1-1
+
+turtle.forward(100)
+
+
+
+
+    
