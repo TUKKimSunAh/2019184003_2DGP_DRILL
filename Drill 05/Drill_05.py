822167665cdad34ecc13e6f4142f330e646e65b6
Sun@MainPC <blaqluv1015@gmail.com>
2022-09-19 23:16:02 +0900

    Submit Drill 05
    Create and Submit Drill05

---
+++ Drill 05/Drill_05.py
@@ -0,0 +1,40 @@
+import turtle
+
+
+def up_turtle():
+    global turlte_dir
+
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+
+def down_turtle():
+    global turlte_dir
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+def left_turtle():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+def right_turtle():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+
+def restart():
+    turtle.reset()
+
+turtle.shape('turtle')
+
+
+turtle.onkey(up_turtle,'w')
+turtle.onkey(down_turtle,'s')
+turtle.onkey(left_turtle,'a')
+turtle.onkey(right_turtle,'d')
+turtle.onkey(restart,'Escape')
+turtle.listen()
